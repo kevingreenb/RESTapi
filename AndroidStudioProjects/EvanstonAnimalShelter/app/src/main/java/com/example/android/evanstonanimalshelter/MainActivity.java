7808d6631ea367f1ff5919815ee0508848fe11da
KGreen <kevin.e.green@hotmail.com>
2018-05-05 21:25:52 -0500

    Second action

--- AndroidStudioProjects/EvanstonAnimalShelter/app/src/main/java/com/example/android/evanstonanimalshelter/MainActivity.java
+++ AndroidStudioProjects/EvanstonAnimalShelter/app/src/main/java/com/example/android/evanstonanimalshelter/MainActivity.java
@@ -5,8 +5,8 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
 import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,44 +19,23 @@ public class MainActivity extends AppCompatActivity {
         twitter = (ImageView) findViewById(R.id.twitter_image);
         facebook = (ImageView) findViewById(R.id.facebook_image);
         instagram = (ImageView) findViewById(R.id.instagram_image);
-    // I welcome recommendations on how to make the code below less repetitive
-        twitter.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String url = (String)v.getTag();
-                Intent intent = new Intent();
-                intent.setAction(Intent.ACTION_VIEW);
-                intent.addCategory(Intent.CATEGORY_BROWSABLE);
-                intent.setData(Uri.parse(url));
-                startActivity(intent);
-            }
-        });
 
-        facebook.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String url = (String)v.getTag();
-                Intent intent = new Intent();
-                intent.setAction(Intent.ACTION_VIEW);
-                intent.addCategory(Intent.CATEGORY_BROWSABLE);
-                intent.setData(Uri.parse(url));
-                startActivity(intent);
-            }
-        });
+    } // End of the onCreate
 
-        instagram.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String url = (String)v.getTag();
-                Intent intent = new Intent();
-                intent.setAction(Intent.ACTION_VIEW);
-                intent.addCategory(Intent.CATEGORY_BROWSABLE);
-                intent.setData(Uri.parse(url));
-                startActivity(intent);
-            }
-        });
+    public void onClick(View view) {
 
+        //Get url from tag
+        String url = (String) view.getTag();
 
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
 
+        //pass the url to intent data
+        intent.setData(Uri.parse(url));
 
+        startActivity(intent);
     }
 
-    
+
 }
